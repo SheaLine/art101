0be98dbcd92d025d7cdeb420861149c4ca713380
SheaLine <sline@ucsc.edu>
2024-10-18 11:29:13 -0700

    Lab 4 first commit

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock, Paper, Scissors
+// An implementation of the game
+// Shea Line <sline@ucsc.edu>
+// October 2024
 
-// Constants
+//* Pseudocode
+// Step 1 - Display game instructions on the screen.
+// Step 2 - Initialize the Game once the player picks settings.
+// Step 3 - Collet player input (have the player choose rock paper or scissors.
+// Step 4 - Randomly generate the computer input (rock paper, or scissors.
+// Step 5 - Determine the winner, check winning conditions.
+// Step 6 - update the score
+// Step 7- Repeat steps 3 - 6 until the player settings are met, (best out of 3, 5, 1)
+
+
+// Print the game instructions to the screen
+// console.log("Press 1 for best out of 1, 3 for best out of 3, or 5 for best out of 5.");
+// while series is not over
+  // get player input, 1 for rock, 2 for paper, 3 for scissors
+      // print rock, paper,  or scissors
+  // get computer input
+      // print rock, paper, or scissors
+  // determine winner
+  // update score
+  // print the score
+// print the winner of the series
+
+// Constants  
 
 // Functions
 
