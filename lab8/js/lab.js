526aa5c1ff06b9117b5390618b895e320f509b37
SheaLine <sline@ucsc.edu>
2024-10-31 13:31:13 -0700

    finished lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -9,17 +9,21 @@ function square(x) {
 }
 
 //function tests
-console.log("The square of 5 is " + square(2));
+console.log("The square of 2 is " + square(2));
 
 //variables
 let numArray = [1, 2, 3, 4, 5];
-console.log(numArray);
+console.log("Base number array: ", numArray);
 
 // Output
 let numArraySquared = numArray.map(square);
-console.log(numArraySquared); // should output [1, 4, 9, 16, 25]
+console.log("Number array squared: ",numArraySquared); // should output [1, 4, 9, 16, 25]
 
 let numArrayCubed = numArray.map(function (x) {
     return x * x * x;
 });
-console.log(numArrayCubed); // should output [1, 8, 27, 64, 125]
+console.log("Number array cubed: ",numArrayCubed); // should output [1, 8, 27, 64, 125]
+
+$("#base-output").append(" ",numArray.join(", "));
+$("#square-output").append(" ",numArraySquared.join(", "));
+$("#cubed-output").append(" ",numArrayCubed.join(", "));
